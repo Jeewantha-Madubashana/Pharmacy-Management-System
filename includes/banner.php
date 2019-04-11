d6cc07a54008d0f2085550c312cb8d8b0764c041
Jeewantha95 <jeewaplayacc@gmail.com>
2019-04-11 14:43:07 +0530

    First Commit

---
+++ includes/banner.php
@@ -0,0 +1,14 @@
+
+            <div class="banner-text" style="margin-top: 40px;height: 220px;">
+                <h3 style="height: 27px;margin-top: -30px;">
+                    WELCOME
+                </h3>
+                <p style="font-weight: italic;font-size: 16px;">
+                    WE ARE THE MEDICINE DISTRIBUTORS<br>
+                     for all branches  <br>
+                    Order medicine and we can provide medicine quickly <br>
+                </p>
+               
+            </div>
+        </div>
+        <!-- //banner -->
---
+++ includes/contact.php
@@ -0,0 +1,20 @@
+<!-- contact -->
+	<div class="contact">
+		<div class="container">				
+			<div class="contact-grids">
+				
+				<div class="col-md-4 address">
+					<h3>Contact info</h3>
+					<p class="cnt-p"> </p>
+					<p>W.G.Supuni Sithara</p>
+					<p>No.58 Medaketiya Tangalle</p>
+					<p>Phone : 0717751700</p>
+					
+					<p>Email : <a href="mailto:example@mail.com">supunisithara123@gmail.com</a></p>
+				</div>									
+				<div class="clearfix"> </div>	
+			</div>
+		
+		</div>		
+	</div>
+	<!--//contact-->
